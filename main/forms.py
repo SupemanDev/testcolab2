7d7a0f6376b27ba95096b48a5a20fd266809f073
A <Ilyas2707@yandex.ru>
2021-06-29 16:55:46 +0300

    'v1.0'

---
+++ main/forms.py
@@ -0,0 +1,22 @@
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea, DateTimeInput
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'task', 'deadline']
+        widgets = {
+            'title': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите название'
+            }),
+            'task': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите описание'
+            }),
+            'deadline': DateTimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите дедлайн'
+            })
+        }
---
+++ main/models.py
@@ -0,0 +1,17 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+    deadline = models.DateTimeField('Дедлайн')
+    # default=datetime.datetime.now
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
